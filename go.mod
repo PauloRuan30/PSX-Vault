63a38eb73e21426a7403756d04e446311da44836
PauloRuan30 <PauloRuan_30@outlook.com>
2026-02-24 10:07:39 -0300

    first main.go commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/PauloRuan30/PSX-Vault
+
+go 1.25.0
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("🚀 PS2Drive v0.1 - Memory Card Online!")
+	fmt.Println("PS2 style file manager backend starting...")
+}
